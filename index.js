a200543ed40239cffa3faa1518157ecbf4d59916
adjieraka006-ship-it <adjieraka006@gmail.com>
2025-09-18 12:55:57 +0700

    Update File

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
-const cetakNama = require('./coba.js')
-console.log (cetakNama('Ajie'))
+// const cetakNama = require('./coba.js')
+// console.log(cetakNama('Tika'))
 
+const fs = require ('fs');
+const coba = require('./coba2.js');
+
+console.log (coba.cetakNama('Ajie'),coba.PI);
